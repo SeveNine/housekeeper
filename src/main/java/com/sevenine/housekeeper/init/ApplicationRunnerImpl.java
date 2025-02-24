e2641aba404b4219d6f9d38008f535e5eacfb942
xjp <935171397@qq.com>
2025-02-24 14:17:16 +0800

    修改

--- src/main/java/com/sevenine/housekeeper/init/ApplicationRunnerImpl.java
+++ src/main/java/com/sevenine/housekeeper/init/ApplicationRunnerImpl.java
@@ -53,6 +53,7 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
         HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
         ResponseEntity<String> resEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
         String body = resEntity.getBody();
+//        System.out.println("body = " + body);
         ResponseDTO responseDTO = JsonUtil.string2Obj(body, ResponseDTO.class);
         List<FundDataDTO> items = responseDTO.getData().getItems();
         items = filter(items);
@@ -70,14 +71,15 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
         System.out.println("指数基金增量定投计划");
         stringBuilder.append("指数基金增量定投计划\n\n");
         // 基金定投起始范围
-        double RANGBEGIN = 15;
+        double RANGBEGIN = 10;
         // 基金定投终止范围
-        double RANGEND = 30;
+        double RANGEND = 20;
         double b = RANGEND;
         double k = (RANGBEGIN - b)/1;
         double x;
         double y;
         double total = 0;
+        double totalMust = 0;
         boolean isCalc = true;
 //        System.out.println("k="+ k + ",b=" +b);
         for (FundDataDTO fund: dto) {
@@ -85,23 +87,42 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
             y = k*x + b;
             double money = Double.parseDouble(new BigDecimal(y).setScale(2,BigDecimal.ROUND_HALF_UP).toString());
 
-            if("上证50".equals(fund.getName())){
-                stringBuilder.append("====下述基金本人暂停买入====\n");
+            if("标普500(050025)".equals(fund.getName())){
+                x = Double.parseDouble(fund.getPe_percentile());
+                double newK = (10 - 100)/1;
+                y = newK*x + 100;
+                money = Double.parseDouble(new BigDecimal(y).setScale(2,BigDecimal.ROUND_HALF_UP).toString());
+            }
+            if("纳斯达克(040046)".equals(fund.getName())){
+                x = Double.parseDouble(fund.getPe_percentile());
+                double newK = (10 - 100)/1;
+                y = newK*x + 100;
+                money = Double.parseDouble(new BigDecimal(y).setScale(2,BigDecimal.ROUND_HALF_UP).toString());
+            }
+
+            if(isCalc){
+                totalMust += money;
+            }
+            total += money;
+            stringBuilder.append(String.format("%-40s ——\t%s \n", fund.getName(), money));
+            if("中国互联(164906)".equals(fund.getName())){
+
+                stringBuilder.append(String.format("%-40s ——\t%s \n", "黄金(000218)", "10"));
+                totalMust += 10;
+                stringBuilder.append("==下方基金根据行情可选买入==\n");
                 isCalc = false;
             }
-            if(isCalc){
-                total += money;
-            }
-            stringBuilder.append(String.format("%-20s ——\t%s \n", fund.getName(), money));
         }
-        stringBuilder.append(String.format("\n总计：%.2f\n\n" ,total));
+        stringBuilder.append(String.format("\n必买合计：%.2f，总计：%.2f\n\n" ,totalMust,total));
+        stringBuilder.append("止盈规则1：每达到30%收益时，赎回盈利部分\n");
+        stringBuilder.append("止盈规则2：收益率超过20%，卖出20%持仓，后续每增加10%收益，都卖出20%持仓\n");
+        stringBuilder.append("定投金额范围："+(int)RANGBEGIN+"-"+(int)RANGEND +"(【纳指100】和【标普500】为10-100)\n");
         stringBuilder.append("作业有风险，抄袭需谨慎！\n");
-        stringBuilder.append("ps:定投金额范围调整为15-30");
         List<String> list = person.getList();
         for(String mail: list){
             mailService.sendSimpleMail(mail,"指数基金增量定投计划",stringBuilder.toString());
         }
-        }
+    }
 
 
 
@@ -112,22 +133,29 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
     public List<FundDataDTO> filter(List<FundDataDTO> dto) {
         List<FundDataDTO> filterList = new LinkedList<>();
         HashMap<String, String> map = new LinkedHashMap<>();
-        map.put("SZ399997","消费(161725)");
-        map.put("SH000991","医疗(003095)");
-        map.put("CSI930652","科技(001410)");
+        map.put("NDX","纳斯达克(040046)");
+        map.put("SP500","标普500(050025)");
+        map.put("SZ399006","创业板(001592)");
         map.put("SZ399417","新能源(002190)");
         map.put("CSIH11136","中国互联(164906)");
-        map.put("NDX","纳斯达克(040046)");
-        map.put("SH000905","中证500");
-        map.put("SZ399006","创业板");
-        map.put("SH000016","上证50");
+//        map.put("","黄金(000218)");
+        // 下方可选买入
+        map.put("SH000016","上证50(110003)");
+        map.put("SH000852","上证1000(013923)");
+        map.put("SH000991","医疗(003095)");
+        map.put("CSI930652","中证电子(001410)");
+        map.put("SZ399997","消费(161725)");
+        map.put("SH000905","中证500(161017)");
         map.put("SH000300","沪深300");
         map.put("SZ399701","基本面60");
         map.put("SZ399975","证券公司");
         map.put("HKHSI","恒生指数");
+        map.put("SZ399393","地产");
+        map.put("SZ399986","银行");
         for (String key : map.keySet()) {
             for (FundDataDTO fund: dto) {
                 if(key.equals(fund.getIndex_code())){
+                    fund.setName(map.get(key));
                     filterList.add(fund);
                 }
             }
--- src/test/java/com/sevenine/housekeeper/HousekeeperApplicationTests.java
+++ src/test/java/com/sevenine/housekeeper/HousekeeperApplicationTests.java
@@ -6,8 +6,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HousekeeperApplicationTests {
 
+    // 止盈计算器
     @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        // 止盈百分比
+        int stopPercent = 20;
+        // 卖出百分比
+        double sellPercent = 20;
+        // 总额
+        double principal = 100;
+        for(int i = stopPercent; i<=100; i+=10){
+            principal = principal - principal * (sellPercent*0.01);
+            System.out.println("盈利："+i+"%,卖出持仓的"+sellPercent+"%,剩余"+(int)principal+"%持仓");
+        }
+    }// 止盈计算器2
+    @Test
+    public static void main2(String[] args) {
+        // 止盈百分比
+        int stopPercent = 20;
+        // 卖出百分比
+        double sellPercent = 30;
+        // 总额
+        double principal = 100;
+        for(int i = stopPercent; i<=100; i+=10){
+            principal = principal - principal * (sellPercent*0.01);
+            System.out.println("盈利："+i+"%,卖出持仓的"+sellPercent+"%,剩余"+(int)principal+"%持仓");
+        }
     }
+
+
+    // 定投倍率计算器
+//    @Test
+//    public static void main(String[] args) {
+//        // 止盈百分比
+//        double power = 5;
+//        // 定投金额
+//        double money = 10;
+//        for(int i = 0; i<=100; i++){
+//            money = money + money * (power*0.01);
+//            System.out.println("pe："+i+"%,增加倍率"+power+"%,定投金额："+(int)money+"");
+//        }
+//    }
+
 
 }
